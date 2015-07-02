05e366d6e7759a33fde67f843cba6ef8e045de30
Hugh Greenish <hugh@monochromechameleon.com>
2015-07-02 10:15:46 +0100

    That's what happens when you are working on two computers simultaneously

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ var defaultConfig = {
 
 // Adds properties of config to base, overwriting as required.
 function apply(config, base) {
-    return _([config]).reduce(function (c, e) { return _.assign(c, e); }, base);
+    return _([config]).reduce(function (c, e) {
+        return _.assign(c, e);
+    }, base);
 }
 
 // Init function can be a string pointing at the config file, an object with additional options, or blank. This
@@ -46,10 +48,10 @@ function applyEnvironmentConfig(config, configConfig) {
 }
 
 function updateReferences(config, configConfig) {
-     // Add properties to initial export.
+    // Add properties to initial export.
     apply(config, initFn);
 
-     // Override export so we get the object directly whenever we subsequently require() this module.
+    // Override export so we get the object directly whenever we subsequently require() this module.
     module.exports = config;
 
     // Declare a global variable if and only if we have explicitly decided to.
@@ -59,7 +61,7 @@ function updateReferences(config, configConfig) {
 }
 
 function load(configConfig) {
-	// Get initial config.
+    // Get initial config.
     var baseConfig = apply(require(configConfig.path), {});
     // Apply environment over the top.
     var config = applyEnvironmentConfig(baseConfig, configConfig);
@@ -70,26 +72,26 @@ function load(configConfig) {
     return config;
 }
 
-var initFn = function (filePathOrConfig) {
+var initFn = function (initOptions) {
 
-	// Process arguments and default config for our actual config import.
+    // Process arguments and default config for our actual config import.
     var opts = handleInitOptions(initOptions);
     var configConfig = apply(opts, defaultConfig);
-    
+
     return load(configConfig);
 };
 
 // On initial load, see whether we have a file in the default location.
 if (fs.existsSync(defaultConfig.path + '.js')) { // fs.exists[Sync] is being deprecated, but stick with it for now.
-	
-	// If that file exists and has a _configConfig property, we assume that that property defines the config behaviour.
-	var tryConfig = require(defaultConfig.path);	
-	var configConfig = apply((tryConfig || {})._configConfig, defaultConfig);
-	
-	// If our config includes an autoload property, just load it and export the config object directly.
-	if (configConfig.autoload === true) {
-		return load(configConfig);
-	}
+
+    // If that file exists and has a _configConfig property, we assume that that property defines the config behaviour.
+    var tryConfig = require(defaultConfig.path);
+    var configConfig = apply((tryConfig || {})._configConfig, defaultConfig);
+
+    // If our config includes an autoload property, just load it and export the config object directly.
+    if (configConfig.autoload === true) {
+        load(configConfig);
+    }
 } else {
-	module.exports = initFn;	
+    module.exports = initFn;
 }
